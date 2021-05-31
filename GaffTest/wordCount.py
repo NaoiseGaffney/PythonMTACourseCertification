302188cfaf30ae2714ff445bd71056c98610d13f
Naoise Olof Seán Gaffney <61478687+NaoiseGaffney@users.noreply.github.com>
2021-05-31 19:13:49 +0100

    Rewriting wordCount.py to make it a little cleaner, and adding the sorted count of words.

--- GaffTest/wordCount.py
+++ GaffTest/wordCount.py
@@ -1,21 +1,27 @@
 # Get the name of the file and open it
-name = input('Enter file:')
-handle = open(name, 'r', encoding="utf-8")
+name = input('Enter file: ').strip()
 
-# Count word frequency
-counts = dict()
-for line in handle:
-    words = line.split()
-    for word in words:
-        counts[word] = counts.get(word,0) + 1
+try:
+    with open(name, 'r', encoding="utf-8") as handle:
+        # Count word frequency
+        counts = {}
+        for line in handle:
+            words = line.split()
+            for word in words:
+                counts[word] = counts.get(word, 0) + 1
 
-# Find the most common word
-bigcount = None
-bigword = None
-for word,count in counts.items():
-    if bigcount is None or count > bigcount:
-        bigword = word
-        bigcount = count
+        # Find the most common word
+        bigcount = None
+        bigword = None
+        for word, count in counts.items():
+            if bigcount is None or count > bigcount:
+                bigword = word
+                bigcount = count
+except FileNotFoundError:
+    print("Please enter a valid filename.")
+    exit(-1)
 
-# All done
-print(bigword, bigcount)
+
+print(f"Most common 'word' is: '{bigword}', and it occurs {bigcount} time(s).")
+tuple_in_right_order = sorted(counts.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+print(tuple_in_right_order)
